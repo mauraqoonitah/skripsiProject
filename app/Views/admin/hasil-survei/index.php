a18754933e4e479ebda20a20180635faa385cd46
mauraqoonitah <mauraqoonitah@gmail.com>
2021-07-31 20:58:06 +0700

    add view modal tambah kategori

--- app/Views/admin/hasil-survei/index.php
+++ app/Views/admin/hasil-survei/index.php
@@ -90,6 +90,14 @@
             myInput.focus()
         })
     </script>
+
+    <!-- popovers -->
+    <script>
+        var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+        var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+            return new bootstrap.Popover(popoverTriggerEl)
+        })
+    </script>
 </body>
 
 </html>
