c28ac52d9132c4b418309e31e4518fe8c9b0f15b
mauraqoonitah <mauraqoonitah@gmail.com>
2021-08-03 18:27:11 +0700

    responsive views landingpage

--- app/Views/Pages/home.php
+++ app/Views/Pages/home.php
@@ -4,25 +4,27 @@
 
 <section class="landing-page">
     <div class="content">
-        <div class="textBox">
-            <h2> <span>Survei Kepuasan 9 Kriteria</span> <br>
-                Penjaminan Mutu Internal<br>
-                <h3>FMIPA UNJ</h3>
-            </h2>
+        <div class="container">
+            <div class="textBox">
+                <h2> <span>Survei Kepuasan 9 Kriteria</span> <br>
+                    Penjaminan Mutu Internal<br>
+                    <h3>FMIPA UNJ</h3>
+                </h2>
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos excepturi omnis placeat nisi corporis quisquam sed tempora nostrum aspernatur ut.</p>
+                <p class="col-lg-12 col-md-10 col-sm-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos excepturi omnis placeat nisi corporis quisquam sed tempora nostrum aspernatur ut.</p>
 
-            <a class="" href="#obyek-menu" role="button">
-                <button type="button" class="btn btn-primary btn-jumbotron ">Isi Survei</button>
-            </a>
+                <a class="" href="#obyek-menu" role="button">
+                    <button type="button" class="btn btn-success btn-jumbotron ">Isi Survei</button>
+                </a>
+            </div>
         </div>
         <div class="imgBox">
-            <img src="<?= base_url(); ?>/img/jumbotron-img.png" alt="" class="jumbotron-image">
+            <img src="<?= base_url(); ?>/img/jumbotron-img.png" alt="" class="img-fluid jumbotron-image">
         </div>
     </div>
     <ul class="thumbnail-bar">
         <li>
-            <img src="<?= base_url(); ?>/img/dot-circle-solid.svg" alt="" width="40" height="40" onclick="itemSlider('<?= base_url(); ?>/img/default.svg');changeCircleColor('#dce775')">
+            <img src="<?= base_url(); ?>/img/dot-circle-solid.svg" alt="" width="40" height="40" onclick="itemSlider('<?= base_url(); ?>/img/default.svg');changeCircleColor('#017143')">
         </li>
         <li>
             <img src="<?= base_url(); ?>/img/chart-bar-solid.svg" alt="" width="40" height="40" onclick="itemSlider('<?= base_url(); ?>/img/undraw_profile_1.svg');changeCircleColor('#64b5f6')">
@@ -34,14 +36,14 @@
 
     <ul class="thumbnail-bar-content">
         <li><a href=""><i class="fas fa-paperclip"></i></a> </li>
-        <li> <a href=""><i class="fas fa-chart-bar"></i></a></li>
+        <li> <a href=""><i class="fas fa-chart-bar "></i></a></li>
         <li><a href=""><i class="fas fa-user"></i></a> </li>
     </ul>
 </section>
 
 <section class="menu">
     <div class="container">
-        <h3 class="section-title " id="obyek-menu">Obyek Instrumen</h3>
+        <h3 class="section-title " id="obyek-menu">Responden Survei</h3>
         <div class="row obyek-menu">
             <a href="./about.php" class="obyek-list col-sm text-reset"> Dosen
             </a>
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,29 +1,30 @@
 <div class="circle-clip-path"></div>
-<nav class="navbar navbar-expand-lg navbar-light bg-light mt-2">
+<nav class="navbar navbar-expand-lg navbar-light mt-2">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="<?= base_url(); ?>/img/unj.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
+            <img src="<?= base_url(); ?>/img/unj.png" alt="" class="d-inline-block align-text-top">
             Sistem Penjaminan Mutu
         </a> <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
             <ul class="navbar-nav ms-auto">
-                <li class="nav-item px-3">
+                <li class="nav-item">
                     <a class="nav-link active" href="<?= base_url(); ?>">Home</a>
                 </li>
-                <li class="nav-item px-3">
+                <li class="nav-item">
                     <a class="nav-link" href="#">About</a>
                 </li>
-                <li class="nav-item px-3">
-                    <a class="nav-link" href="<?= base_url('admin'); ?>"><?php if (logged_in()) : ?>Admin</a>
+                <li class="nav-item">
+                    <?php if (logged_in()) : ?>
+                        <a class=" nav-link" href="<?= base_url('admin'); ?>"> Admin</a>
                 </li>
-                <li class="nav-item px-3">
+                <li class="nav-item">
                     <a class="nav-link" href="<?= base_url('logout'); ?>">Logout</a>
                 <?php else : ?>
                 </li>
-                <li class="nav-item px-3">
-                    <a class="nav-link" href="<?= base_url('ogin'); ?>">Login</a>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= base_url('login'); ?>">Login</a>
                 <?php endif; ?>
                 </li>
 
