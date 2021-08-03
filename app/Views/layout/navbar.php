c3f50c4c8bd28b23b1a8bf78967f445869666fe0
mauraqoonitah <mauraqoonitah@gmail.com>
2021-08-03 16:57:10 +0700

    change navbar to bootstrap code

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,36 +1,33 @@
 <div class="circle-clip-path"></div>
-
-<nav class="navbar navbar-expand-lg navbar-light ">
+<nav class="navbar navbar-expand-lg navbar-light bg-light mt-2">
     <div class="container">
-        <a class="navbar-brand" href="/">
-            <img src="<?= base_url(); ?>/img/unj.png" alt="" width="40" height="40">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <a class="navbar-brand" href="#">
+            <img src="<?= base_url(); ?>/img/unj.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
+            Sistem Penjaminan Mutu
+        </a> <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+        <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item px-3">
+                    <a class="nav-link active" href="<?= base_url(); ?>">Home</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link" href="<?= base_url('admin'); ?>"><?php if (logged_in()) : ?>Admin</a>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link" href="<?= base_url('logout'); ?>">Logout</a>
+                <?php else : ?>
+                </li>
+                <li class="nav-item px-3">
+                    <a class="nav-link" href="<?= base_url('ogin'); ?>">Login</a>
+                <?php endif; ?>
+                </li>
 
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav text-white ms-auto">
-                <ul>
-                    <li><a class="nav-link active" aria-current="page" href="<?= base_url(); ?>">Beranda</a></li>
-                    <li><a class="nav-link" href="../pages/about">About</a></li>
-                    <li>
-                        <?php if (logged_in()) : ?>
-                            <a class="nav-link" href="<?= base_url('admin'); ?>">Admin</a>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="<?= base_url('logout'); ?>">Logout</a>
-                    <?php else : ?>
-                    </li>
-                    <li>
-                        <a class="nav-link" href="<?= base_url('login'); ?>">Login</a>
-                    <?php endif; ?>
-                    </li>
-                </ul>
-
-            </div>
+            </ul>
         </div>
-
     </div>
-
 </nav>
--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -5,15 +5,14 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Favicon-->
     <link href="<?= base_url(); ?>/img/unj.png" rel="shortcut icon" type="image/icon" />
     <!-- Font Awesome -->
     <link rel="stylesheet" href="<?= base_url(); ?>/../../plugins/fontawesome-free/css/all.min.css">
-    <!-- bootstrap css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- icon bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- my css -->
     <link rel="stylesheet" href="<?= base_url(); ?>/css/style.css">
 
