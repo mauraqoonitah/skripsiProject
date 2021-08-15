8bd6f9e444619b415eff542c555151c320d09bde
mauraqoonitah <mauraqoonitah@gmail.com>
2021-08-15 22:05:21 +0700

    applying opentab pilih kategori for page kelola butir

--- app/Views/admin/kelola-survei/pernyataan.php
+++ app/Views/admin/kelola-survei/pernyataan.php
@@ -18,208 +18,67 @@
 
             </div>
         </div><!-- /.container-fluid -->
-        <!-- flash success tambah data  -->
-        <?php if (session()->getFlashdata('msgButir')) :  ?>
-            <div class="alert alert-success d-flex align-items-center fw-bold mb-3" role="alert">
-                <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-                    <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
-                    </symbol>
-                </svg>
-                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:">
-                    <use xlink:href="#check-circle-fill" />
-                </svg>
-                <div>
-                    <?= session()->getFlashData('msgButir'); ?>
-                </div>
-            </div>
-        <?php endif; ?>
-        <!-- ./ flash success tambah data  -->
     </section>
 
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <div class="card">
-                <div class="alert alert-info" role="alert">
-                    Pilih instrumen untuk melihat butir pernyataan
-                </div>
-                <!-- butir pernyataan per instrumen -->
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <!-- datatables -->
-                        <table id="table-kelola-instrumen" class="stripe hover row-border" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th>No.</th>
-                                    <th>Instrumen</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php $i = 1; ?>
-                                <?php foreach ($instrumen as $ins) : ?>
-                                    <tr>
-                                        <td><?= $i++; ?></td>
-                                        <td><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="nanti masuk ke detail lihat semua butir pada instrumen ini"><?= $ins['namaInstrumen']; ?></a></td>
-                                        <td>
-                                            <div class="btn-group" role="group">
-                                                <a href="<?= base_url(); ?>/admin/tambahPernyataan" class="ml-auto mr-2" data-bs-toggle="tooltip" data-bs-placement="top" title="Tambah Pernyataan">
-                                                    <button type="button" class="btn btn-warning ">
-                                                        <i class="fas fa-plus"></i> Tambah
-                                                    </button>
-                                                </a>
-                                                <a href="<?= base_url(); ?>/admin/editButirPernyataan/id" class="btn btn-sm btn-warning text-decoration-none" data-bs-toggle="tooltip" data-bs-placement="top" title="Lihat/Edit">
-                                                    <i class="fas fa-edit"></i>
-                                                </a>
-                                                <button type="button" class="btn btn-sm btn-danger" data-bs-placement="top" title="Hapus" data-bs-toggle="modal" data-bs-target="#hapusButir">
-                                                    <i class="fas fa-trash-alt"></i>
-                                                </button>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                <!-- ./butir pernyataan per instrumen -->
+    <div class="content col-lg-12 mx-auto">
+        <div class=" container-fluid">
+            <strong>Pilih Kode Kategori</strong><br>
+            <button class="tablink" onclick="openPage('defaultTab')" id="defaultOpen">Default Tab Open</button>
 
+            <?php foreach ($pernyataan as $butir) : ?>
+                <button class="tablink" onclick="openPage('Kode-<?= $butir['id']; ?>')"><?= $butir['kodeCategory']; ?></button>
+            <?php endforeach; ?>
+
+            <div id="defaultTab" class="tabcontent">
             </div>
         </div>
-    </section>
-    <!-- /.content -->
-    <br><br><br><br><br>
-    <hr>
-    <hr>
-    <hr>
-    <hr>
-    <hr>
-    <!-- butir pernyataan per instrumen -->
-    <div class="card-body">
-        <div class="table-responsive">
-            <!-- datatables -->
-            <table id="table-kelola-instrumen" class="stripe hover row-border" style="width:100%">
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>Butir Pernyataan</th>
-                        <th>Aksi</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $i = 1; ?>
-                    <?php foreach ($pernyataan as $q) : ?>
-                        <tr>
-                            <td><?= $i++; ?></td>
-                            <td><?= $q['pernyataan']; ?></td>
-                            <td>
-                                <div class="btn-group" role="group">
-                                    <a href="<?= base_url(); ?>/admin/editButirPernyataan/<?= $q['id']; ?>" class="btn btn-sm btn-warning text-decoration-none" data-bs-toggle="tooltip" data-bs-placement="top" title="Lihat/Edit">
-                                        <i class="fas fa-edit"></i>
-                                    </a>
-                                    <button type="button" class="btn btn-sm btn-danger" data-bs-placement="top" title="Hapus" data-bs-toggle="modal" data-bs-target="#hapusButir">
-                                        <i class="fas fa-trash-alt"></i>
-                                    </button>
+    </div>
+
+    <?php foreach ($pernyataan as $butir) : ?>
+        <div id="Kode-<?= $butir['id']; ?>" class="tabcontent">
+
+            <section class="content col-lg-8 mx-auto d-flex align-items-center" style=" min-height: 30vh">
+                <div class=" container-fluid">
+                    <p><?= $butir['namaInstrumen']; ?></p>
+                    <div class="card">
+                        <div class="alert alert-info text-center fw-bold" role="alert">
+                            <strong> Pilih instrumen untuk melihat butir pernyataan </strong>
+                        </div>
+
+                        <div class="card-body mx-auto col-lg-12 align-middle">
+                            <div class="container">
+                                <div class="input-group mb-3">
+                                    <select class="form-select form-select-lg" aria-label=".form-select-lg example">
+                                        <option value="<?= $butir['namaInstrumen']; ?>"><?= $butir['namaInstrumen']; ?></option>
+                                    </select>
+                                    <span class="input-group-append">
+                                        <a href="<?= base_url(); ?>/admin/listInstrumen/<?= $butir['id']; ?>" class="btn btn-info btn-flat">Next</a>
+                                    </span>
                                 </div>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <!-- ./butir pernyataan per instrumen -->
-
-</div>
-
-<!-- modal tambah butir -->
-<div class="modal fade" id="tambahButirModal" tabindex="-1" aria-labelledby="tambahButirLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="tambahButirLabel">Tambah Butir</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <!-- form tambah butir -->
-                <form action="<?= base_url(); ?>/admin/savePernyataan" method="post">
-                    <?= csrf_field(); ?>
-
-                    <!-- pilih kode kategori -->
-                    <div class="form-group">
-                        <div class="mb-3 row">
-                            <label for="kode-kategori" class="col-sm-2 col-form-label">Kategori:</label>
-                            <div class="col-sm-10">
-                                <select class="form-select" id="kodeCategory" name="kodeCategory">
-                                    <?php foreach ($category as $ctg => $value) :  ?>
-                                        <option value="<?= $value['kodeCategory']; ?>"><?= $value['kodeCategory']; ?> - <?= $value['namaCategory']; ?></option>
-                                    <?php endforeach; ?>
-                                </select>
+                                <!-- /input-group -->
                             </div>
                         </div>
                     </div>
+                </div>
 
-                    <!-- nama instrumen -->
-                    <div class="form-group">
-                        <div class="mb-3 row">
-                            <label for="nama-instrumen" class="col-sm-2 col-form-label">Nama instrumen:</label>
-
-                            <div class="col-sm-10">
-                                <select class="form-select" id="peruntukkan-kategori">
-                                    <option selected class="text-muted">Pilih </option>
-                                    <option value="">ins2 </option>
-                                    <option value=""> ins1</option>
-                                    <option value="">ins3</option>
-                                    <option value="">ins4</option>
-
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- isi butir -->
-                    <div class="form-group">
-                        <div class="mb-3 row">
-                            <label for="pernyataan" class="col-sm-2 col-form-label">Isi Butir Pernyataan:</label>
-                            <div class="col-sm-10">
-                                <textarea class="form-control" id="pernyataan" name="pernyataan" rows="3"></textarea>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                        <button type="submit" class="btn btn-success">Simpan</button>
-                    </div>
-                </form>
-                <!-- end form tambah butir -->
-            </div>
-
+            </section>
         </div>
-    </div>
+    <?php endforeach; ?>
 </div>
-<!-- end modal tambah butir -->
 
-
-<!-- modal hapus Butir Pernyataan -->
-<div class="modal fade" id="hapusButir" tabindex="-1" aria-labelledby="hapusButirLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered ">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title fw-bold" id="hapusButirLabel">Hapus Butir Pernyataan</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body text-center">
-                <h5>Yakin hapus butir pernyataan ini?</h5>
-                <i class="fas fa-exclamation-circle fa-3x" style="width: 3rem; color: #D60C0C"></i>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batalkan</button>
-                <button type="button" class="btn btn-danger">Hapus</button>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- end modal hapus butir -->
+<script>
+    function openPage(pageName) {
+        // Hide all elements with class="tabcontent" by default */
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        // Show the specific tab content
+        document.getElementById(pageName).style.display = "block";
+    }
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+</script>
 
 <?= $this->endSection(); ?>
